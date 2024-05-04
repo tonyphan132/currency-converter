01f54d264e92c57afa443d9adac819e97217f33a
Tony Phan <tonyphanmc@hotmail.com>
2024-05-04 16:35:30 -0400

    add currency

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,26 @@
 package org.example;
 
 import com.google.gson.Gson;
+import java.net.http.HttpRequest;
+import java.util.Scanner;
 
 public class Main {
+    private final static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
         apiKeyClass.readKey();
-        String url = "https://api.currencyapi.com/v3/latest?apikey=" + apiKeyClass.getApiKey();
-        System.out.println(url);
-        System.out.println("Hello world!");
+        String route = "https://api.currencyapi.com/v3/latest?apikey=" + apiKeyClass.getApiKey();
+        displayCurrency();
+        in.close();
+    }
 
+    private static void getParam(){
+        System.out.println("What is your base currency?");
+        displayCurrency();
+        System.out.println("What currency are you converting into?");
+        displayCurrency();
+    }
+
+    private static void displayCurrency(){
+        System.out.println("CAD: Canadian Dollar\nVND: Vietnamese Dong\nUSD: US Dollar\n");
     }
 }
